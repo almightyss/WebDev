2c3bd6c284ce686a17a4ba88c50a9ea873c4e6d2
almightyss <rauanfaizullayev@gmail.com>
2023-04-06 03:09:42 +0600

    lab

---
+++ Week8/shop_back/api/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    category = models.IntegerField()
+    is_active = models.BooleanField()
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'category': self.category,
+            'is_active': self.is_active
+        }
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
